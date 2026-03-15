58f0a8fbb13ad6bba7f994a21c63b14c6198b233
UndeadAnarchist <tibirium458@gmail.com>
2026-03-15 21:21:23 +0300

    Update level1_video.jl

--- level1_video.jl
+++ level1_video.jl
@@ -10,16 +10,15 @@ typeof(my_pi) #если не писать type of, чего будет
 my_name = "Tiba"
 typeof(my_name)
 my_answer = my_pi
-#=большие коммы
+большие коммы
 =#
-=#
-#sum = 3+7
-#=a = 2
+sum = 3+7
+a = 2
 b = 4
-#typeof(a)
-#typeof(b)
+typeof(a)
+typeof(b)
 sum = a+b
-#println(sum)
+println(sum)
 difference = b-a
 product = a * b
 quotient = b/a
@@ -64,56 +63,56 @@ push!(fibonacci, 21)
 fibonacci
 pop!(fibonacci)
 fibonacci
-=#
-#favs = [["eggs", "dogs"],["hedges","two eggs"]]
-#numbers = [[1,2,3],[4,5],[6,7,8,9]]
-#rand(4,3)
-#rand(4,3,2)
 
-#=n = 0
+favs = [["eggs", "dogs"],["hedges","two eggs"]]
+numbers = [[1,2,3],[4,5],[6,7,8,9]]
+rand(4,3)
+rand(4,3,2)
+
+n = 0
 while n < 10
     n += 1
     println(n)
 end
-=#
-#=friends = ["Balgor", "Balgor 2.0", "Balgor 3.0"]
+
+friends = ["Balgor", "Balgor 2.0", "Balgor 3.0"]
 i = 1
 while i <= length(friends)
     friend = friends
     println("Hi $friend, I`m so tired")
     i += 1
-end=#
-#=for m in 1:10
+end
+for m in 1:10
     println(m)
 end
 friends = ["Balgor", "Balgor 2.0", "Balgor 3.0"]
 for friend in friends
     println("aaaaaaaa, my $friend")
-end=#
-#=m, n =3, 2
+end
+m, n =3, 2
 A = zeros(m, n)
 for i in 1:m
     for j in 1:n
 A[i, j] = i + j
     end    
 end
-A=#
-#=B = zeros(m, n)
+A
+B = zeros(m, n)
 for i in 1:m, j in 1:n
     B[i, j] = i + j
 end
-B=#
-#C = [i + j for i in 1:m, j in 1:n]
-#C
-#=for n in 1:10
+B
+C = [i + j for i in 1:m, j in 1:n]
+C
+for n in 1:10
     A = [i + j for i in 1:n, j in 1:n]
     display(A)
 end
-A =#
+A 
 
-#x = 25
-#y = 8
-#=
+x = 25
+y = 8
+
 if x > y
     println("$x > $y ")
 elseif y > x
@@ -121,20 +120,20 @@ elseif y > x
 else 
     println("$x = $y")
 end
-=#
-#=
+
+
 if x > y 
     x
 else
     y 
-end=#
+end
 
-#(x > y) ? x : y
+(x > y) ? x : y
 
-#(x > y) && println("$x > $y")
-#(x < y) && println("$y > $x")
+(x > y) && println("$x > $y")
+(x < y) && println("$y > $x")
 
-#=function sayhi(name)
+function sayhi(name)
     println("hi $name, lalala")
 end
 
@@ -149,24 +148,24 @@ f(42)
 sayhi(name) = println("hi $name, f")
 f2(x)=x^2
 sayhi2("steal")
-f2(42)=#
+f2(42)
 
-#=sayhi3 = name -> println("Stop $name")
+sayhi3 = name -> println("Stop $name")
 f3 = x -> x^2
 sayhi("Chyi")
-f3(42)=#
-#=
+f3(42)
+
 sayhi(234646493180)
 A = rand(3, 3)
 f(A)
 
 v = rand(3)
-=#
-#v = [3, 5, 2]
-#sort(v)
-#sort!(v)
-#v
-#=
+
+v = [3, 5, 2]
+sort(v)
+sort!(v)
+v
+
 A = [i + 3*j for j in 0:2, i in 1:3]
 f(A)
 B=f.(A)
@@ -175,7 +174,7 @@ A[2, 2]^2
 B[2, 2]
 v = [1, 2, 3]
 f.(v)
-=#
+
 
 #Pkg.add("Example")
 #using Example
@@ -185,10 +184,10 @@ f.(v)
 #using Colors
 #palette = distinguishable_colors(100)
 #rand(palette, 3, 3)
-#=using Pkg
+using Pkg
 Pkg.add("Plots")
 using Plots
-#=
+
 x= -3:0.1:3
 f(x) = x^2
 
@@ -197,9 +196,9 @@ gr()
 plot(x, y, label="line")
 scatter!(x, y, label="points")
 
-plotlyjs()=#
+plotlyjs()
 
-#=globaltemperatures = [14.4, 14.5, 14.8, 15.2, 15.5, 15.8]
+globaltemperatures = [14.4, 14.5, 14.8, 15.2, 15.5, 15.8]
 
 numpirates = [45000, 20000, 15000, 5000, 400, 17]
 plot(numpirates,globaltemperatures,legend=false)
@@ -207,21 +206,21 @@ scatter!(numpirates,globaltemperatures,legenf=false)
 xflip!()
 xlabel!("Number of pirates")
 ylabel!("Global Temperature")
-title!("Influence of pirate population on global warming")=#
+title!("Influence of pirate population on global warming")
 
 p1 = plot(x, x)
 p2 = plot(x, x.^2)
 p3 = plot(x, x.^3)
 p4 = plot(x, x.^4)
 plot(p1,p2,p3,p4,layout=(2,2),legend=false)
-title!("i hate my life")=#
-#=
+title!("i hate my life")
+
 methods(+)
-#=@which 3+3
+@which 3+3
 @which 3.0+3.0
 @which 3+3.0
 import Base: +
-"hello"+"world!"=#
+"hello"+"world!"
 +(x::String, y::String) = string(x, y)
 "hello" + " world!"
 @which "hello"+"world"
@@ -232,13 +231,13 @@ foo(x::Int, y::Int)=println("foo with 2 integers floats")
 foo(1,1)
 foo(1., 1.)
 foo(1, 1.0)
-foo(true,false)=#
+foo(true,false)
 
 A = rand(1:4,3,3)
 B=A
 C=copy(A)
 [B C]
-#=
+
 A[1]=17
 [B C]
 
@@ -250,10 +249,10 @@ A\b
 
 Atall=A[:,1:2]
 display(Atall)
-Atall\b=#
-#=
+Atall\b
+
 A=randn(3,3)
-[A[:,1] A[:,1]]\b=#
+[A[:,1] A[:,1]]\b
 
 Ashort=A[1:2,:]
 display(Ashort)
